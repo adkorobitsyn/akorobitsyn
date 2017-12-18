b08b5d0c8ec0bd23accde00e7706ccef6595e2e8
AlogyKoR <alogy@bk.ru>
2017-12-18 23:13:07 +0300

    1.1. Создание Hello World[#141]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,10 +1,10 @@
 package ru.job4j;
 
-public class Calculate
-{
-	public 	static void main(String[] args)
-	{
+public class Calculate{
+	
+	public 	static void main(String[] args){
 		System.out.println("Hello world!");
 	}
+	
 }
 		
