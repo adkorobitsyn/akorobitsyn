6e85f2341070f2f5e13b628b27138bf965f0ae38
Alexey Korobitsyn <alogy@bk.ru>
2018-01-13 23:01:19 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+*JavaDoc для класса Calculate
+*
+*@author Alexey Korobitsyn (korobitsyn-ad@ya.ru)
+*/
+
 public class Calculate{
+	
+	/** Метод main 
+	*выводит на экран надпись 
+	*"Hello World!"
+	*/
 	
 	public 	static void main(String[] args){
 		System.out.println("Hello world!");
