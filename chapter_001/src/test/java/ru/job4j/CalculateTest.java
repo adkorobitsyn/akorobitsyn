30e7a17a1e43840a1d63076f03480bc19b7c2245
Alexey Korobitsyn <alogy@bk.ru>
2018-01-15 22:41:16 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,21 +7,21 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Alexey Korobitsyn (korobitsyn-ad@ya.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Alexey Korobitsyn (korobitsyn-ad@ya.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-	/**
-	* Test echo.
-	*/ @Test
-	public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Alexey Korobitsyn";
-    String expect = "Echo, echo, echo : Alexey Korobitsyn"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-	}
+    /**
+     * Test echo.
+     */ @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Alexey Korobitsyn";
+        String expect = "Echo, echo, echo : Alexey Korobitsyn";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 }
