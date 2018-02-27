1a25ed726613628dd6cba206e0021f31b0e71716
Alexey Korobitsyn <alogy@bk.ru>
2018-02-27 19:35:27 +0300

    3.3. Идеальный вес. [#41584]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -9,12 +9,27 @@ public class Point {
     private int x;
     private int y;
 
-    public Point(int x, int y) {
+    private Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public double distanceTo(Point that) {
-        return -1;
+    private double distanceTo(Point that) {
+        return Math.sqrt(
+                Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
+        );
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(10, 20);
+        Point b = new Point(1, 2);
+        // сделаем вызов метода
+        System.out.println("x1 = " + a.x);
+        System.out.println("y1 = " + a.y);
+        System.out.println("x2 = " + b.x);
+        System.out.println("y2 = " + b.y);
+
+        double result = a.distanceTo(b);
+        System.out.println("Расстояние между точками А и В : " + result);
     }
 }
