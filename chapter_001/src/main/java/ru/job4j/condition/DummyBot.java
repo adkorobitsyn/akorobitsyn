7b8a83e4c5450731011cb3d43fbbc57b1998afe6
Alexey Korobitsyn <alogy@bk.ru>
2018-02-27 21:00:58 +0300

    3.3. Идеальный вес. [#41584]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+/**
+ * @author Alexey Korobitsyn (alogy@bk.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class DummyBot {
+    /**
+     * Отвечает на вопросы
+     * @param question Вопрос от клиента.
+     * @return Ответ
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            // Заменить на правильный ответ rsl = "Привет, умник.".
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) { // Заменить на проверку, что этот вопрос извествен боту и он знает как на него ответить.
+            // заменить на правильный ответ rsl = "До скорой встречи.".
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
